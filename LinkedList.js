8f426cd4c856f49f7541b711176ba6ecc9bcaebe
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-02-21 10:29:13 +0200

    Add append method

--- LinkedList.js
+++ LinkedList.js
@@ -1,9 +1,21 @@
-import {Node} from "./Node.js";
+import { Node } from "./Node.js";
 
-export class LinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }    
+export class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(value) {
+    if (this.size === 0) {
+      this.head = new Node(value);
+      this.tail = this.head;
+    } else {
+      const node = new Node(value);
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.size++;
+  }
 }
